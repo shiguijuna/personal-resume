38b53415575a232ad0bba122292085c30973884c
shiguijuna <shiguijuna@qq.com>
2026-05-24 13:36:46 +0800

    fix: useHead provide context missing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createHeadCore, VueHeadMixin } from '@unhead/vue'
+import { createHeadCore } from '@unhead/vue'
 import router from './router'
 import App from './App.vue'
 import './assets/styles/base.scss'
@@ -8,9 +8,16 @@ import './assets/styles/base.scss'
 const app = createApp(App)
 const head = createHeadCore()
 
+const headPlugin = {
+  install() {
+    app.config.globalProperties.$unhead = head
+    app.config.globalProperties.$head = head
+    app.provide('usehead', head)
+  },
+}
+
 app.use(createPinia())
-app.use(head as any)
-app.mixin(VueHeadMixin)
+app.use(headPlugin)
 app.use(router)
 
 app.mount('#app')
